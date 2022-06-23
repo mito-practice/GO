838d15e7a5b04c833072ef9852074ad75f2a6bc6
blackturtle71 <glebosadchiy71@gmail.com>
2022-06-23 20:57:39 +0200

    rename variables

--- homework/hw1/1/nucl_seq_generator.py
+++ homework/hw1/1/nucl_seq_generator.py
@@ -7,8 +7,8 @@ seq = ''.join(random.choices('ATGC', k=length))
 result_handler = NCBIWWW.qblast("blastn", "nr", seq, format_type='Text')
 print(seq)
 print(result_handler)
-with open('blasta_res.txt', 'w') as save_file:
+with open('blasta_res.txt', 'w') as fout:
     blast_res = result_handler.read()
-    save_file.write(blast_res)
+    fout.write(blast_res)
 
     
